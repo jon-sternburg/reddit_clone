4ca6d7707f843c81425390d6a568aac9ce052a0e
jon-sternburg <jonsternburg@gmail.com>
2023-08-29 13:00:27 -0400

    switched to img from nextjs Image comp - too slow

--- src/app/components/Clicked_Mobile_Post.tsx
+++ src/app/components/Clicked_Mobile_Post.tsx
@@ -17,8 +17,6 @@ h_: number
 i: number
 post: Thread
 w_: number
-clicked: boolean
-prev?: string
 }
 
 type MediaSize = {
@@ -31,24 +29,22 @@ handle_link_click: (e:MouseEvent<HTMLAnchorElement>, url:string) => void
 height: number
 data: PostChildData
 width: number
-clicked: boolean
 }
 
 type Text_Inner_Props = {
 height: number
 data: PostChildData
-clicked: boolean
 }
 
 
-export default function Mobile_Post(props: Post_Props):JSX.Element {
+export default function Clicked_Mobile_Post(props: Post_Props):JSX.Element {
 const router = useRouter()
 const pathname = usePathname()
 const {post, h_} = props
 const sub_icon = post.data.sr_detail && post.data.sr_detail.icon_img.length > 0 ? post.data.sr_detail.icon_img : post.data.sr_detail && post.data.sr_detail.community_icon ? post.data.sr_detail.community_icon.replace('amp;', '') : null
-const type_ = (post.data.is_video)|| (post.data.post_hint && post.data.post_hint.includes('video')) ? <Video_ clicked = {props.clicked} handle_link_click = {handle_link_click} width = {props.w_} height = {h_} data = {post.data} /> : 
-                                                                                post.data.is_self ? <Text_ clicked = {props.clicked} data = {post.data}  height = {h_} />  : 
-                                                                                <Image_ clicked = {props.clicked} handle_link_click = {handle_link_click} height = {h_} data = {post.data} width = {props.w_} /> 
+const type_ = (post.data.is_video)|| (post.data.post_hint && post.data.post_hint.includes('video')) ? <Video_ handle_link_click = {handle_link_click} width = {props.w_} height = {h_} data = {post.data} /> : 
+                                                                                post.data.is_self ? <Text_ data = {post.data}  height = {h_} />  : 
+                                                                                <Image_ handle_link_click = {handle_link_click} height = {h_} data = {post.data} width = {props.w_} /> 
 
 function handle_link_click(e:MouseEvent<HTMLAnchorElement>, url:string) {
 e.preventDefault()
@@ -75,7 +71,7 @@ return (
 <header className = {styles.post_box_top_wrapper_mobile} >
 <div className = {styles.post_box_subreddit_mobile} onClick = {(e) => handle_post_box_click(e)}>
 <Link href={`/r/${post.data.subreddit}`}>
-{post.data.sr_detail && sub_icon !== null ? <img alt = {"subreddit icon image"} height = {20} width = {20} style= {{marginRight: '5px', borderRadius: '100%'}} src = {sub_icon} className = {styles.icon_img} />
+{post.data.sr_detail && sub_icon !== null ? <Image alt = {"subreddit icon image"} height = {20} width = {20} style= {{marginRight: '5px', borderRadius: '100%'}} src = {sub_icon} className = {styles.icon_img} />
 :
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className = {styles.generic_icon} >
 <path d="M16.5,2.924,11.264,15.551H9.91L15.461,2.139h.074a9.721,9.721,0,1,0,.967.785ZM8.475,8.435a1.635,1.635,0,0,0-.233.868v4.2H6.629V6.2H8.174v.93h.041a2.927,2.927,0,0,1,1.008-.745,3.384,3.384,0,0,1,1.453-.294,3.244,3.244,0,0,1,.7.068,1.931,1.931,0,0,1,.458.151l-.656,1.558a2.174,2.174,0,0,0-1.067-.246,2.159,2.159,0,0,0-.981.215A1.59,1.59,0,0,0,8.475,8.435Z"></path>
@@ -85,10 +81,6 @@ return (
 <span>r/{post.data.subreddit}</span>
 </Link>
 </div>
-
-
-
-
 <div className = {styles.post_box_title_wrap_mobile}>
 <h3 className = {styles.post_box_title}>{post.data.title}</h3>
 </div>
@@ -97,15 +89,6 @@ return (
 <div className = {styles.post_box_inner_mobile}>
 {type_}
 </div>
-{props.clicked && (
-<div className={styles.post_box_author_mobile} onClick={(e) => handle_post_box_click(e)}>
-{post.data.author == '[deleted]' ?
-  <div>Posted by {post.data.author}<span style={{ marginLeft: '2px' }}> {String.fromCharCode(183)}{` ${post.posted_time}`}</span></div>
-  :
-  <Link href={`/u/${post.data.author}`}>Posted by {post.data.author}<span style={{ marginLeft: '2px' }}> {String.fromCharCode(183)}{` ${post.posted_time}`}</span></Link>
-}
-</div>
-)}
 <div className = {styles.post_box_bottom_wrap_mobile}>
 
 <div className = {styles.post_box_score}>
@@ -117,7 +100,6 @@ return (
 <FaRegComment className = {styles.comments_icon} /> 
 <span>{post.data.num_comments}</span>
 </div>
-
 </div>
 </article>
 
@@ -127,18 +109,17 @@ function Image_(props: Post_Inner_Props): JSX.Element {
 const [img_error, set_img_error] = useState<boolean>(false)
 const [dim, set_dim] = useState<MediaSize>({w: null, h: null})
 let src_ =  props.data.url 
-let styles_ = props.clicked ? { maxHeight: props.height * .7 } : {maxHeight: props.height * .5}
+
 function onError() {
 set_img_error(true)
 }
 
 
-
   useEffect(() => {
     function getMediaSize(iw:number, ih:number):MediaSize {
       let r_ = props.width <= 800 ? .9 : .4
-      let max_h = props.clicked ? props.height * .7 : props.height * .6
-       let max_w = props.width * r_
+      let max_h = props.height * .7
+      let max_w = props.width * r_
       
       let widthPercent = max_w / iw;
       let heightPercent = max_h / ih;
@@ -157,8 +138,7 @@ let prev_w = props.data.preview.images[0].source.width
 let dim = getMediaSize(prev_w, prev_h)
 set_dim(dim)
 } 
-}, [props.width, props.height, props.data.preview, props.data.url, props.clicked])
-
+}, [props.width, props.height, props.data.preview, props.data.url])
 
 
   return (
@@ -170,7 +150,7 @@ set_dim(dim)
     autoPlay 
     loop 
     muted
-    style = {styles_}
+    style = {{maxHeight: props.height * .5}}
     controls
     preload = {'auto'} 
     className = {styles.video_post} 
@@ -190,8 +170,8 @@ set_dim(dim)
 :
 <Fragment>
 {dim.w && dim.h && (
- <img alt = {'post image'} 
- className = {props.clicked ? styles.img_post_clicked : styles.img_post} 
+ <Image alt = {'post image'} 
+ className = {styles.img_post} 
  src = {src_} 
  loading = {"lazy"}
  width = {dim.w}
@@ -220,9 +200,9 @@ set_dim(dim)
 if (props.data.selftext && props.data.selftext.length > 0) { 
 
 let self_text_ = props.data.selftext.replace(/&amp;#x200B;/g, '')
-let styles_ = props.clicked ?  {} : {maxHeight: props.height * .3}
+
   return (
-<div className = {props.clicked ? styles.selftext_box : styles.selftext_box_feed} style = {styles_} >
+<div className = {styles.selftext_box_feed} style = {{maxHeight: props.height * .3}} >
 <ReactMarkdown remarkPlugins={[remarkGfm]}>
 {self_text_}
 </ReactMarkdown>
@@ -239,7 +219,6 @@ props.data.url.includes('gfy') ? 'gfy' : props.data.url.includes('stream') ? 'st
 let src_ = type_ == 'youtube' || type_ == 'redgifs'  || type_ == 'gfy' ? props.data.secure_media_embed.media_domain_url : props.data.secure_media && props.data.secure_media.reddit_video && props.data.secure_media.reddit_video.fallback_url ?
 props.data.secure_media.reddit_video.fallback_url  : props.data.url
 
-let styles_ = props.clicked ? { maxHeight: props.height } : {maxHeight: props.height * .5}
   return (
     <Fragment>
 
@@ -257,7 +236,7 @@ let styles_ = props.clicked ? { maxHeight: props.height } : {maxHeight: props.he
     autoPlay 
     loop 
     muted
-    style = {styles_}
+    style = {{maxHeight: props.height * .5}}
     controls
     preload = {'none'} 
     className = {styles.video_post} 
--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -82,7 +82,7 @@ return (
 <div className = {styles.post_info_wrapper}>
 <div className = {styles.post_box_subreddit} onClick = {(e) => handle_post_box_click(e)}>
 <Link href={`/r/${post.data.subreddit}`}>
-{post.data.sr_detail && sub_icon !== null ? <Image alt = {"subreddit icon image"} height = {20} width = {20} style= {{marginRight: '5px', borderRadius: '100%'}} src = {sub_icon} className = {styles.icon_img} />
+{post.data.sr_detail && sub_icon !== null ? <img alt = {"subreddit icon image"} height = {20} width = {20} style= {{marginRight: '5px', borderRadius: '100%'}} src = {sub_icon} className = {styles.icon_img} />
 :
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" className = {styles.generic_icon} >
 <path d="M16.5,2.924,11.264,15.551H9.91L15.461,2.139h.074a9.721,9.721,0,1,0,.967.785ZM8.475,8.435a1.635,1.635,0,0,0-.233.868v4.2H6.629V6.2H8.174v.93h.041a2.927,2.927,0,0,1,1.008-.745,3.384,3.384,0,0,1,1.453-.294,3.244,3.244,0,0,1,.7.068,1.931,1.931,0,0,1,.458.151l-.656,1.558a2.174,2.174,0,0,0-1.067-.246,2.159,2.159,0,0,0-.981.215A1.59,1.59,0,0,0,8.475,8.435Z"></path>
@@ -198,7 +198,7 @@ set_dim(dim)
 :
 <Fragment>
 {dim.w && dim.h && (
- <Image alt = {'post image'} 
+ <img alt = {'post image'} 
  className = {props.clicked ? styles.img_post_clicked : styles.img_post} 
  src = {src_} 
  loading = {"lazy"}
